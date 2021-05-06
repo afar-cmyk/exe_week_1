08849e4438dc8f4b45b72025368717fa461c381c
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-05-06 16:11:43 -0500

    Condicionales de cifras y mensaje final

--- Exercise_1/Ex01.py
+++ Exercise_1/Ex01.py
@@ -12,3 +12,26 @@ resuInt = numHoras * costoHoras
 
 ## conversion del resultado a string para agregar el punto decimal y el apostrofe de millón
 resuStr = list(str(resuInt))
+
+if len(resuStr) == int(4):
+    resuStr.insert(1, '.')
+    resultado = "".join(resuStr)
+
+elif len(resuStr) == int(5):
+    resuStr.insert(2, '.')
+    resultado = "".join(resuStr)
+
+elif len(resuStr) == int(6):
+    resuStr.insert(3, '.')
+    resultado = "".join(resuStr)
+
+elif len(resuStr) == int(7):
+    resuStr.insert(1, "'")   
+    resuStr.insert(5, '.')
+    resultado = "".join(resuStr)
+
+else:
+    resultado = "".join(resuStr)
+
+## mensaje final con el resultado de la operación
+print (f"El pago para el trabajador es de: {resultado} cop.")
