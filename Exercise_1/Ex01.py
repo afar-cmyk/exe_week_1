bb549ca8ddc6d8788a059e66a4100f026b7dbc98
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-05-06 15:23:40 -0500

    variables del programa

--- Exercise_1/Ex01.py
+++ Exercise_1/Ex01.py
@@ -2,3 +2,7 @@
 #
 #   Programa que sirve para calcular el pago en pesos 
 #   colombianos a un trabajador conforme a sus horas trabajadas
+
+## Ingreso de datos a las variables del programa
+numHoras = int(input("Ingrese el numero de horas trabajadas: "))
+costoHoras = int(input("Ingrese el costo por hora del trabajador: "))
