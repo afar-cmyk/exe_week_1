3009a8d456e41db0c019cfd0afe120518be71bf5
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-05-06 16:09:34 -0500

    Integración de mas variables

--- Exercise_1/Ex01.py
+++ Exercise_1/Ex01.py
@@ -6,3 +6,9 @@
 ## Ingreso de datos a las variables del programa
 numHoras = int(input("Ingrese el numero de horas trabajadas: "))
 costoHoras = int(input("Ingrese el costo por hora del trabajador: "))
+
+## calculo de los datos ingresados
+resuInt = numHoras * costoHoras
+
+## conversion del resultado a string para agregar el punto decimal y el apostrofe de millón
+resuStr = list(str(resuInt))
